928012e626d337656013335ab519015daaec8e9f
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-07 11:15:46 +0200

    fix: Corregido error de Redux sobrante

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -1,2 +1,1 @@
 export * from './login.actions';
-export * from './perfil.actions';
--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import credenciales from './datosLogin.reducer';
-import datosPerfil from './datosPerfil.reducer';
+
 
 const rootReducer = combineReducers({
     credenciales,
-    datosPerfil,
+    
 });
 
 export default rootReducer;
