af268147c22d9eac595d0285bb31a3e7574b7229
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-09 23:20:00 +0200

    feat: Añadida la funcionalidad de enviar Mensaje en el archivo Chat.jsx

--- src/Containers/Chat/Chat.jsx
+++ src/Containers/Chat/Chat.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Chat.css';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
@@ -17,13 +16,20 @@ import 'moment/locale/es';
 
 
 const Chat = (props) => {
-    let navigate = useNavigate();
+
     const [mensajes, setMensajes] = useState([]);
+    const [datosUsuario, setDatosUsuario] = useState({
+        mensaje: "",
+    });
 
     useEffect(() => {
         verMensajes();
         moment.locale('es');
     }, []);
+
+    const rellenarDatos = (e) => {
+        setDatosUsuario({ ...datosUsuario, [e.target.name]: e.target.value })
+    };
 
     const verMensajes = async () => {
         let config = {
@@ -32,11 +38,29 @@ const Chat = (props) => {
         try {
             let resultado = await axios.get(`${baseURL}/mensajes/comunidad/${props.datosComunidad.id}`, config);
             setMensajes(resultado.data);
-            console.log('Estos son los resultados', resultado.data);
         } catch (error) {
             console.log(error)
         }
     };
+
+    const enviarMensaje = async () => {
+        let body = {
+            comunidad_id: props.datosComunidad.id,
+            miembro_id: props.credenciales.usuario.id,
+            mensaje: datosUsuario.mensaje,
+            fecha: Date.now(),
+            hora: moment().format('LT'),
+        };
+        let config = {
+            headers: { Authorization: `Bearer ${props.credenciales.token}` }
+        };
+        try {
+            let respuesta = await axios.post(`${baseURL}/mensajes/miembro`, body, config);
+            console.log(respuesta.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className='paginaChat'>
@@ -44,26 +68,27 @@ const Chat = (props) => {
             <div className="contenidoChat">
                 <div className="cardChat">
                     <Card style={{ width: '100rem', height: '50rem', backgroundColor: '#272b30' }}>
-                        <Card.Body style={{ color: '#fff', overflow: 'scroll' }}>
-                            <Card.Title style={{ color: '#fff' }}>Chat : {props.datosComunidad.titulo}</Card.Title>
+                        <Card.Body className='CardBody' style={{ color: '#fff', overflow: 'auto',  }}>
+                            <Card.Title style={{ color: '#fff' }}>Chat de la Comunidad de {props.datosComunidad.titulo}</Card.Title>
                             {
                                 mensajes.map((mensaje) => {
                                     return (
                                         <div className='chat' key={mensaje.id}>
                                             <Card.Text style={{ color: '#fff' }}>{props.credenciales.usuario.nick}</Card.Text>
                                             <Card.Text style={{ color: '#fff' }}>{mensaje.mensaje}</Card.Text>
-                                            <Card.Text style={{ color: '#fff' }}>{mensaje.fecha}</Card.Text>
+                                            <Card.Text style={{ color: '#fff' }}>{moment(mensaje.fecha).format('LL')}</Card.Text>
                                             <Card.Text style={{ color: '#fff' }}>{mensaje.hora}</Card.Text>
+                                            <hr></hr>
                                         </div>
                                     )
                                 })
                             }
                         </Card.Body>
                         <Form>
-                            <Form.Group className="mb-3" controlId="formBasicText">
-                                <Form.Control style={{ width: '80rem', height: '3em', marginLeft: '9.5em' }} type="text" placeholder="Mensaje" />
+                            <Form.Group className="mb-3" controlId="mensaje">
+                                <input className='inputChat' type="text" name="mensaje" id="mensaje" title="mensaje" placeholder="Escribe..." autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                             </Form.Group>
-                            <Button style={{ marginLeft: '9.5em', marginBlockEnd: '1em' }} variant="secondary">Enviar Mensaje</Button>
+                            <Button onClick={() => enviarMensaje()} style={{ marginLeft: '9.5em', marginBlockEnd: '1em' }} variant="secondary">Enviar Mensaje</Button>
                         </Form>
                     </Card>
                 </div>
