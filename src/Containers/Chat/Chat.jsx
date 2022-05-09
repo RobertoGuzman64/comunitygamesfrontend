06a5a3451f6f346ca6f47a26d340a44054db401a
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-09 19:47:45 +0200

    feat: Añadido el mapeo de Mensajes de Comunidad en el archivo Chat.jsx

--- src/Containers/Chat/Chat.jsx
+++ src/Containers/Chat/Chat.jsx
@@ -18,12 +18,25 @@ import 'moment/locale/es';
 
 const Chat = (props) => {
     let navigate = useNavigate();
+    const [mensajes, setMensajes] = useState([]);
 
-    const cambiarPagina = (pagina) => {
-        setTimeout(() => {
-            navigate(pagina)
-        }, 500);
-    }
+    useEffect(() => {
+        verMensajes();
+        moment.locale('es');
+    }, []);
+
+    const verMensajes = async () => {
+        let config = {
+            headers: { Authorization: `Bearer ${props.credenciales.token}` }
+        };
+        try {
+            let resultado = await axios.get(`${baseURL}/mensajes/comunidad/${props.datosComunidad.id}`, config);
+            setMensajes(resultado.data);
+            console.log('Estos son los resultados', resultado.data);
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
         <div className='paginaChat'>
@@ -31,18 +44,26 @@ const Chat = (props) => {
             <div className="contenidoChat">
                 <div className="cardChat">
                     <Card style={{ width: '100rem', height: '50rem', backgroundColor: '#272b30' }}>
-                        <Card.Body>
-                            <Card.Title style={{ color: '#fff' }}>Chat</Card.Title>
-                            <Card.Text style={{ color: '#fff' }}>
-                                Aquí podrás interactuar con los usuarios de la aplicación.
-                            </Card.Text>
+                        <Card.Body style={{ color: '#fff', overflow: 'scroll' }}>
+                            <Card.Title style={{ color: '#fff' }}>Chat : {props.datosComunidad.titulo}</Card.Title>
+                            {
+                                mensajes.map((mensaje) => {
+                                    return (
+                                        <div className='chat' key={mensaje.id}>
+                                            <Card.Text style={{ color: '#fff' }}>{props.credenciales.usuario.nick}</Card.Text>
+                                            <Card.Text style={{ color: '#fff' }}>{mensaje.mensaje}</Card.Text>
+                                            <Card.Text style={{ color: '#fff' }}>{mensaje.fecha}</Card.Text>
+                                            <Card.Text style={{ color: '#fff' }}>{mensaje.hora}</Card.Text>
+                                        </div>
+                                    )
+                                })
+                            }
                         </Card.Body>
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicText">
-                                <Form.Control style={{ width: '80rem', height: '3em', marginLeft:'9.5em' }} type="text" placeholder="Mensaje" />
+                                <Form.Control style={{ width: '80rem', height: '3em', marginLeft: '9.5em' }} type="text" placeholder="Mensaje" />
                             </Form.Group>
-                            
-                            <Button style={{ marginLeft:'9.5em',marginBlockEnd:'1em' }}variant="secondary">Enviar Mensaje</Button>
+                            <Button style={{ marginLeft: '9.5em', marginBlockEnd: '1em' }} variant="secondary">Enviar Mensaje</Button>
                         </Form>
                     </Card>
                 </div>
