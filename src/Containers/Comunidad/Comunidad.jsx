9071729647391d903e39f4072870ffc8a6ebf4c3
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-12 00:07:51 +0200

    feat: Añadida nueva función para poder ver el boton de unirse a la comunidad si no eres miembro en el archivo Comunidad.jsx

--- src/Containers/Comunidad/Comunidad.jsx
+++ src/Containers/Comunidad/Comunidad.jsx
@@ -10,7 +10,7 @@ import axios from 'axios';
 import { baseURL } from '../../utiles';
 // Redux
 import { connect } from 'react-redux';
-import { DATOS_MIEMBRO } from '../../redux/actions';
+import { MIEMBROS_COMUNIDAD } from '../../redux/actions';
 // Moment
 import moment from 'moment';
 import 'moment/locale/es';
@@ -19,6 +19,7 @@ import 'moment/locale/es';
 const Comunidad = (props) => {
     let navigate = useNavigate();
     const [miembros, setMiembros] = useState([]);
+
 
     const cambiarPagina = (pagina) => {
         setTimeout(() => {
@@ -30,9 +31,6 @@ const Comunidad = (props) => {
         verMiembros();
         moment.locale('es');
     }, []);
-
-    useEffect(() => {
-    }, [miembros]);
 
     useEffect(() => {
         if (props.credenciales.token === '') {
@@ -49,91 +47,72 @@ const Comunidad = (props) => {
         try {
             let resultado = await axios.get(`${baseURL}/miembros/comunidad/${props.datosComunidad.id}`, config);
             setMiembros(resultado.data);
-            props.dispatch({ type: DATOS_MIEMBRO, payload: resultado.data });
+            props.dispatch({ type: MIEMBROS_COMUNIDAD, payload: resultado.data });
         } catch (error) {
             console.log(error)
         }
     };
-    if (props.datosMiembro.miembro === props.credenciales.usuario.id) {
-        console.log('DATOSMIEMBRO1', props.datosMiembro)
-        return (
-            <div className='paginaComunidad'>
-                <Header />
-                <div className="contenidoComunidad">
-                    <div className="cardComunidad">
-                        <Card >
-                            <Card.Img variant="top" src={
-                                props.datosComunidad.imagen === undefined ? 'https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-15.jpg' : props.datosComunidad.imagen
-                            } />
-                            <Card.Body>
-                                <Card.Title>Miembros de esta Comunidad{ }</Card.Title>
-                                {
-                                    miembros.map((miembro) => {
-                                        return (
-                                            <div className='datosMiembros' key={miembro.id}>
-                                                <Card.Text>{miembro.nick}</Card.Text>
-                                                <img className='avatarMiembro' src={miembro.avatar} alt={miembro} />
-                                                <Card.Text>Miembro desde : {moment(miembro.fecha).format('LL')}</Card.Text>
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <Button onClick={() => cambiarPagina("/Comunidades")} variant="outline-secondary">Volver</Button>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Button onClick={() => cambiarPagina("/Chat")} variant="secondary">Chat de la Comunidad</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                </div>
-                <Footer />
-            </div>
-        )
+
+    const mostrarBoton = () => {
+        if (props.datosMiembro.length > 0) {
+            for (let i = 0; i < props.datosMiembro.length; i++) {
+                if (props.datosMiembro[i].usuario_id === props.credenciales.usuario.id) {
+                    return (
+                        <></>
+                    )
+                }
+            }
+            return (
+                <Button onClick={() => cambiarPagina("/Miembro")} variant="secondary">Unirse a la Comunidad</Button>
+            )
+        } else {
+            return (
+                <Button onClick={() => cambiarPagina("/Miembro")} variant="secondary">Unirse a la Comunidad</Button>
+            )
+        }
     }
-    else {
-        console.log('DATOSMIEMBRO2', props.datosMiembro)
-        return (
-            <div className='paginaComunidad'>
-                <Header />
-                <div className="contenidoComunidad">
-                    <div className="cardComunidad">
-                        <Card >
-                            <Card.Img variant="top" src={
-                                props.datosComunidad.imagen === undefined ? 'https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-15.jpg' : props.datosComunidad.imagen
-                            } />
-                            <Card.Body>
-                                <Card.Title>Miembros de esta Comunidad{ }</Card.Title>
-                                {
-                                    miembros.map((miembro) => {
-                                        return (
-                                            <div className='datosMiembros' key={miembro.id}>
-                                                <hr></hr>
-                                                <Card.Text>{miembro.nick}</Card.Text>
-                                                <img className='avatarMiembro' src={miembro.avatar} alt={miembro} />
-                                                <Card.Text>Miembro desde : {moment(miembro.fecha).format('LL')}</Card.Text>
-                                                <hr></hr>
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <Button onClick={() => cambiarPagina("/Comunidades")} variant="outline-secondary">Volver</Button>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Button onClick={() => cambiarPagina("/Miembro")} variant="secondary">Unirse a la Comunidad</Button>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <Button onClick={() => cambiarPagina("/Chat")} variant="secondary">Chat de la Comunidad</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
+
+    return (
+        <div className='paginaComunidad'>
+            <Header />
+            <div className="contenidoComunidad">
+                <div className="cardComunidad">
+                    <Card >
+                        <Card.Img variant="top" src={
+                            props.datosComunidad.imagen === undefined ? 'https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-15.jpg' : props.datosComunidad.imagen
+                        } />
+                        <Card.Body>
+                            <Card.Title>Miembros de esta Comunidad{ }</Card.Title>
+                            {
+                                miembros.map((miembro) => {
+                                    return (
+                                        <div className='datosMiembros' key={miembro.id}>
+                                            <Card.Text>{miembro.nick}</Card.Text>
+                                            <img className='avatarMiembro' src={miembro.avatar} alt={miembro} />
+                                            <Card.Text>Miembro desde : {moment(miembro.fecha).format('LL')}</Card.Text>
+                                        </div>
+                                    )
+                                })
+                            }
+                            <Button onClick={() => cambiarPagina("/Comunidades")} variant="outline-secondary">Volver</Button>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            {mostrarBoton()}
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Button onClick={() => cambiarPagina("/Chat")} variant="secondary">Chat de la Comunidad</Button>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <Footer />
             </div>
-        )
-    }
+            <Footer />
+        </div>
+    )
 }
+
 
 
 
 export default connect((state) => ({
     credenciales: state.credenciales,
     datosComunidad: state.datosComunidad.comunidad,
-    datosMiembro: state.datosMiembro.miembro
+    datosMiembro: state.datosMiembro.miembros
 }))(Comunidad);
