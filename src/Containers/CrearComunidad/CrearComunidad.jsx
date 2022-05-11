e2920401e003754244ebe9eaab7cc4eec01a1090
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-11 11:09:56 +0200

    feat: Añadida funcionalidad y diseño de la vista CrearComunidad

--- src/Containers/CrearComunidad/CrearComunidad.jsx
+++ src/Containers/CrearComunidad/CrearComunidad.jsx
@@ -4,46 +4,80 @@ import './CrearComunidad.css';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 // Axios
 import axios from 'axios';
 import { baseURL } from '../../utiles';
+// Redux
+import { connect } from 'react-redux';
 
-const CrearComunidad = () => {
+
+const CrearComunidad = (props) => {
+    let navigate = useNavigate();
+
+    const [datosUsuario, setDatosUsuario] = useState({
+        titulo: "",
+        imagen: "",
+        genero: "",
+        fecha: "",
+        popularidad: "",
+        descripcion: "",
+    });
+
+    useEffect(() => {
+        if (props.credenciales.usuario.administrador === false) {
+            navigate('/');
+        }
+    })
+
+    const rellenarDatos = (e) => {
+        setDatosUsuario({
+            ...datosUsuario,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const crearNuevaComunidad = async () => {
+        let body = {
+            titulo: datosUsuario.titulo,
+            imagen: datosUsuario.imagen,
+            genero: datosUsuario.genero,
+            fecha: datosUsuario.fecha,
+            popularidad: datosUsuario.popularidad,
+            descripcion: datosUsuario.descripcion,
+        }
+        let config = {
+            headers: { Authorization: `Bearer ${props.credenciales.token}` }
+        };
+        try {
+            let resultado = await axios.post(`${baseURL}/comunidades`, body, config);
+            console.log(resultado.data);
+            navigate("/Comunidades");
+        } catch (error) {
+            console.log(error, 'error');
+        }
+    }
+
     return (
         <div className='paginaCrearComunidad'>
             <Header />
             <div className="contenidoCrearComunidad">
                 <div className="inputsCrearComunidad">
-                    <input className='input' type="text" name="nick" id="nick" title="nick" placeholder="Elige un nick de Jugador (ejemplo, el de Discord)" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="text" name="nombre" id="nombre" title="nombre" placeholder="Nombre:" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="text" name="apellidos" id="apellidos" title="apellidos" placeholder="Apellidos:" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="date" name="edad" id="edad" title="edad" placeholder="Fecha de Nacimiento" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="email" name="email" id="email" title="email" placeholder="Correo Electronico" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="password" name="clave" id="clave" title="clave" placeholder="Elige una Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="text" name="discord" id="discord" title="discord" placeholder="Introduce Tu ID de Discord (hazte una cuenta si no tienes)" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <input className='input' type="text" name="juego" id="juego" title="juego" placeholder="Introduce Tu Juego Favorito" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                    <Button onClick={() => crearComunidad()} variant="secondary" size="lg">
-                        Crear Comunidad
+                    <input className='input' type="text" name="titulo" id="titulo" title="titulo" placeholder="Nombre del Juego" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="text" name="imagen" id="imagen" title="imagen" placeholder="URL de la imagen de la portada del Juego" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="text" name="genero" id="genero" title="genero" placeholder="Género del Juego ejemplos : (guerra) (estrategia)etc..." autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="date" name="fecha" id="fecha" title="fecha" placeholder="Fecha de Lanzamiento del Juego" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="float" name="popularidad" id="popularidad" title="popularidad" placeholder="Popularidad del Juego" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="text" name="descripcion" id="descripcion" title="descripcion" placeholder="Descripción completa del Juego" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <Button onClick={() => crearNuevaComunidad()} variant="secondary" size="lg">
+                        Crear Comunidad del Juego
                     </Button>
                 </div>
             </div>
             <Footer />
         </div>
     )
-
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-export default CrearComunidad;
+export default connect((state) => ({
+    credenciales: state.credenciales,
+}))(CrearComunidad);
