b8386e91453dd61c9b2de1604d28dcc244888494
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-12 16:42:24 +0200

    feat: Añadidos mensajes de error en la vista de Registro.jsx y añadido al archivo utiles.jsx los controles de email y nombre de Usuario

--- src/Containers/Registro/Registro.jsx
+++ src/Containers/Registro/Registro.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Registro.css';
+import { checkError } from '../../utiles';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import Button from 'react-bootstrap/Button'
@@ -12,7 +13,7 @@ import { baseURL } from '../../utiles';
 
 const Registro = () => {
     let navigate = useNavigate();
-
+    const [ msgError, setMsgError ] = useState('');
     const [datosUsuario, setDatosUsuario] = useState({
         nick: "",
         nombre: "",
@@ -20,18 +21,34 @@ const Registro = () => {
         edad: "",
         email: "",
         clave: "",
+        clave2: "",
         discord: "",
         juego: "",
     });
 
     const rellenarDatos = (e) => {
-        setDatosUsuario({
-            ...datosUsuario,
-            [e.target.name]: e.target.value
-        })
+        setDatosUsuario({...datosUsuario,[e.target.name]: e.target.value});
     };
 
-    const registrame = async (props) => {
+    const registrame = async () => {
+        setMsgError('');
+        let error = '';
+        let inputsUsuario = Object.entries(datosUsuario);
+
+        if(datosUsuario.clave !== datosUsuario.clave2){
+            return (setMsgError('Las dos contraseñas deben de coincidir'));
+        } else {
+            setMsgError('');
+        }
+
+        for(let elemento of inputsUsuario){
+            error = checkError( elemento[0], elemento[1] );
+            if(error !== 'ok'){
+                setMsgError(error);
+                return;
+            };
+        };
+
         let body = {
             nick: datosUsuario.nick,
             nombre: datosUsuario.nombre,
@@ -43,8 +60,7 @@ const Registro = () => {
             juego: datosUsuario.juego,
         }
         try {
-            let resultado = await axios.post(`${baseURL}/usuarios`, body);
-            console.log(resultado.data);
+            await axios.post(`${baseURL}/usuarios`, body);
             setTimeout(() => {
                 navigate("/login");
             }, 500);
@@ -64,8 +80,10 @@ const Registro = () => {
                     <input className='input' type="date" name="edad" id="edad" title="edad" placeholder="Fecha de Nacimiento" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                     <input className='input' type="email" name="email" id="email" title="email" placeholder="Correo Electronico" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                     <input className='input' type="password" name="clave" id="clave" title="clave" placeholder="Elige una Contraseña" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    <input className='input' type="password" name="clave2" id="clave2" title="clave2" placeholder="Repite la Contraseña por favor" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                     <input className='input' type="text" name="discord" id="discord" title="discord" placeholder="Introduce Tu ID de Discord (hazte una cuenta si no tienes)" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                     <input className='input' type="text" name="juego" id="juego" title="juego" placeholder="Introduce Tu Juego Favorito" autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                    {msgError}
                     <Button onClick={() => registrame()} variant="secondary" size="lg">
                         Regístreme
                     </Button>
@@ -74,7 +92,6 @@ const Registro = () => {
             <Footer />
         </div>
     )
-
 }
 
 export default Registro;
--- src/utiles.jsx
+++ src/utiles.jsx
@@ -1,1 +1,22 @@
 export const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/';
+
+export const checkError = (type,value) => {
+    switch(type) {
+        case 'email' :
+            if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value) ) {
+                
+                return "Introduce un e-mail válido";
+            }else{
+                return "ok";
+            };
+        case 'nombre': 
+            if (! /[a-z]/gi.test(value) ) {
+                return "Introduce un nombre válido";
+            }else{
+                return "ok";
+            };
+        default:
+            return "ok";
+        
+    }
+};
