5f0eed77cc9df5c70ea2f5da06f2f0c74ceb22f3
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-15 17:42:30 +0200

    fix: Corregido el cambio de Redux previamente hecho

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { applyMiddleware,  } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { save, load } from "redux-localstorage-simple";
 import reducer from './reducers';
 
-const configureStoreWithMiddleware = applyMiddleware(
+const createStoreWithMiddleware = applyMiddleware(
 	save({ states: [ 'credenciales', 'datosComunidad', 'datosMiembro' ] })
-)(configureStore);
+)(createStore);
 
-const store = configureStoreWithMiddleware(
+const store = createStoreWithMiddleware(
     reducer,
     load({ states: [ 'credenciales', 'datosComunidad', 'datosMiembro' ] }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
