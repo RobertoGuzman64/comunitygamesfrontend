7e02edfb8f2162e264173c504f31bed6b1d2464f
RobertoGuzman <robertoguzman64@gmail.com>
2022-05-16 00:48:15 +0200

    feat: Añadido control sobre Administrador en la vista BorrarComunidad

--- src/Containers/BorrarComunidad/BorrarComunidad.jsx
+++ src/Containers/BorrarComunidad/BorrarComunidad.jsx
@@ -15,18 +15,48 @@ import { connect } from 'react-redux';
 const BorrarComunidad = (props) => {
     let navigate = useNavigate();
 
+    const cambiarPagina = (pagina) => {
+        setTimeout(() => {
+            navigate(pagina)
+        }, 500);
+    }
+
+    useEffect(() => {
+        if (props.credenciales.usuario.administrador === false) {
+            navigate('/');
+        };
+    })
+
+    useEffect(() => {
+    }, [props.datosComunidad.comunidad])
+
+    const eliminarComunidad = async () => {
+        let config = {
+            headers: { Authorization: `Bearer ${props.credenciales.token}` }
+        };
+        try {
+            let respuesta = await axios.delete(`${baseURL}/comunidades/${props.datosComunidad.comunidad.id}`, config);
+            if (respuesta.status === 200) {
+                navigate('/Comunidades');
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div className='paginaEditarClave'>
+        <div className='paginaBorrarComunidad'>
             <Header />
-            <div className='contenidoEditarClave'>
-                <div className='inputsEditarClave'>
-                    <p className='titulo'>Seguro que quieres eliminar tu cuenta ??</p>
-                    <div className='botonesEditarClave'>
-                        <Button onClick={() => cambiarPagina("/EditarPerfil")} variant="outline-secondary" size="lg">
+            <div className='contenidoBorrarComunidad'>
+                <div className='inputsBorrarComunidad'>
+                    <p className='titulo'>Seguro que quieres eliminar {props.datosComunidad.comunidad.titulo} ??</p>
+                    <p className='titulo'></p>
+                    <div className='botonesBorrarComunidad'>
+                        <Button onClick={() => cambiarPagina("/Comunidades")} variant="outline-secondary" size="lg">
                             Volver
                         </Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <Button onClick={() => borrarUsuario()} variant="danger" size="lg">
-                            Eliminar cuenta
+                        <Button onClick={() => eliminarComunidad()} variant="danger" size="lg">
+                            Eliminar Comunidad
                         </Button>
                     </div>
                 </div>
@@ -37,5 +67,6 @@ const BorrarComunidad = (props) => {
 }
 
 export default connect((state) => ({
+    credenciales: state.credenciales,
     datosComunidad: state.datosComunidad
 }))(BorrarComunidad);
